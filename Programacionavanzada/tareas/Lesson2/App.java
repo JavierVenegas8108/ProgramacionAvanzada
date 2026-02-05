8edfaf384455861557e8cb15fe9efa68ad26e612
JavierVenegas <javiervene24@gmail.com>
2026-02-04 23:31:31 -0600

    Create App.java

---
+++ Programacionavanzada/tareas/Lesson2/App.java
@@ -0,0 +1,21 @@
+package Clases;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame("Hello World");
+				frame.setSize(600,500);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+
+}
+
+
