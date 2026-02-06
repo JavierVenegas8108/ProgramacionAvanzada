1a168dad6baefa3bc765ac677759a4b4eb50a062
JavierVenegas <javiervene24@gmail.com>
2026-02-05 21:18:40 -0600

    Add files via upload

---
+++ Programacionavanzada/tareas/Lesson9/FormPanel.java
@@ -0,0 +1,15 @@
+package Clases;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class FormPanel extends JPanel{
+
+	public FormPanel() {
+		Dimension dim = getPreferredSize();
+		dim.width= 250;
+		setPreferredSize(dim);
+		
+	}
+}
